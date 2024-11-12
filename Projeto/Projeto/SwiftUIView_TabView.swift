7793ad61ad1819a3420ad886b83767d5226e6a9b
icaro <164355795+icarolindogostoso@users.noreply.github.com>
2024-11-12 17:28:06 -0300

    Add files via upload

---
+++ Projeto/Projeto/SwiftUIView_TabView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct SwiftUIView_TabView: View {
+    @State var TabView_nome: String = ""
+    var body: some View {
+        ZStack{
+            TabView{
+                SwiftUIView_Home().tabItem {
+                    Label("primeiro", systemImage: "house")
+                }
+                SwiftUIView_Armario().tabItem {
+                    Label("segundo", systemImage: "house")
+                }
+                SwiftUIView_Amigos(Amigos_nome: TabView_nome).tabItem { // adicionem as outras telas aqui, por favor
+                    Label("terceiro", systemImage: "house")
+                }
+              
+            }.tint(.black)  //.toolbarBackground(Color.yellow, for: .tabBar)
+            //        .onAppear(){
+            //            UITabBar.appearance().backgroundColor = .blue // isso aqui vai mudar a cor da tabview, alguem acha a cor certa
+            //        }.tint(.black)
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    SwiftUIView_TabView()
+}
