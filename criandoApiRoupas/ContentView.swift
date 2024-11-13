ae352a911a9146fbad44da4217dfadabcb69814a
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-11-13 15:04:47 -0300

    adicionando get

--- criandoApiRoupas/ContentView.swift
+++ criandoApiRoupas/ContentView.swift
@@ -1,26 +1,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var vm = ViewModel()
     @State private var nome: String = ""
     @State private var cor: String = ""
     @State private var tipo: String = ""
     var body: some View {
         VStack {
+            ForEach(vm.roupas, id: \.self){ r in
+                HStack{
+                    
+                    Text(r.nome!).foregroundColor(.black)
+                    Spacer()
+                    
+                }
+            }
+            
+            
             TextField(
-                    "Digite o tipo",
-                    text: $tipo
-                )
+                "Digite o tipo",
+                text: $tipo
+            )
             TextField(
-                    "Digite o nome",
-                    text: $nome
-                )
+                "Digite o nome",
+                text: $nome
+            )
             TextField(
-                    "Digite a cor",
-                    text: $cor
-                )
-            Button(action: {post(Roupas(tipo:tipo, nome: nome, cor: cor))}, label: {
+                "Digite a cor",
+                text: $cor
+            )
+            Button(action: {vm.post(Roupas(tipo:tipo, nome: nome, cor: cor))}, label: {
                 Text("btn")
             })
+            
+        }.onAppear(){
+            vm.fetch()
             
         }
         .padding()
--- criandoApiRoupas/metodoPost.swift
+++ criandoApiRoupas/metodoPost.swift
@@ -1,50 +1,65 @@
 import Foundation
 
 
-struct Roupas: Encodable, Hashable{
+struct Roupas: Codable, Hashable{
     let tipo: String?
     let nome: String?
     let cor: String?
 }
 
-
-func post(_ obj: Roupas){ //Aqui deve ser colocado o nome da Struct relacionada
+class ViewModel : ObservableObject {
+    @Published var roupas : [Roupas] = []
     
-    guard let url = URL(string: "http://192.168.128.77:1880/postRoupas") else { return }
-    
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    
-    var headers = request.allHTTPHeaderFields ?? [:]
-    headers["Content-Type"] = "application/json"
-    request.allHTTPHeaderFields = headers
-    
-    let encoder = JSONEncoder()
-    
-    do {
-        let jsonData = try encoder.encode(obj)
-        request.httpBody = jsonData
-        print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
-    } catch {
-        print("Error encoding to JSON: \(error.localizedDescription)")
+    func fetch()
+    {
+        let task = URLSession.shared.dataTask(with: URL(string:"http://192.168.128.77:1880/getRoupas")!){ data, _, error in
+            do {
+                self.roupas = try JSONDecoder().decode([Roupas].self, from: data!)
+            }catch{
+                print(error)
+            }
+        }
+        task.resume()
     }
-    
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        if let error = error {
-            print("Error to send resource: \(error.localizedDescription)")
-            return
+    func post(_ obj: Roupas){
+        
+        guard let url = URL(string: "http://192.168.128.77:1880/postRoupas") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        var headers = request.allHTTPHeaderFields ?? [:]
+        headers["Content-Type"] = "application/json"
+        request.allHTTPHeaderFields = headers
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let jsonData = try encoder.encode(obj)
+            request.httpBody = jsonData
+            print("jsonData: ", String(data: request.httpBody!, encoding: .utf8) ?? "no body data")
+        } catch {
+            print("Error encoding to JSON: \(error.localizedDescription)")
         }
         
-        guard let httpResponse = response as? HTTPURLResponse else {
-            print("Error to send resource: invalid response")
-            return
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error to send resource: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Error to send resource: invalid response")
+                return
+            }
+            
+            if httpResponse.statusCode == 200 {
+                print("Resource POST successfully")
+            } else {
+                print("Error POST resource: status code \(httpResponse.statusCode)")
+            }
         }
-        
-        if httpResponse.statusCode == 200 {
-            print("Resource POST successfully")
-        } else {
-            print("Error POST resource: status code \(httpResponse.statusCode)")
-        }
+        task.resume()
     }
-    task.resume()
 }
+
